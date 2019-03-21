9d24070c7bb2096c9a67391fbf4155519baec794
Wasif Rahman <wasifrhmn2@gmail.com>
2019-03-21 01:10:26 -0400

    Added layout changes to fit more displays, Need to add mobile displays soon.

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function toggleOpen() {
 }
 
 cards.forEach(card => card.addEventListener("click", toggleOpen));
+
+var request = new XMLHttpRequest();
+
+const apikey = "?apikey=4520AB42-F31C-4BF3-80C8-01CE5AD0AB99";
+
+const assets = "https://rest.coinapi.io/v1/assets";
+
+request.open("GET", assets + apikey, true);
+
+request.onload = function() {
+	var data = JSON.parse(this.response);
+	console.log(data);
+	// data.forEach(asset => {
+	// 	console.log(asset.asset_id);
+	// });
+};
+
+request.send();
